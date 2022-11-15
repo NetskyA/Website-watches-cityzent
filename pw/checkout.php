484f210ebef8b6c1fcb5cf54f864f0b79e6d2127
Aldi <aldiaafn16@gmail.com>
2022-11-15 22:48:13 +0700

    tambah form

--- pw/checkout.php
+++ pw/checkout.php
@@ -52,10 +52,7 @@
                                     </svg></a>
                             </li>
                             <li class="nav-item dropdown me-4 pt-2">
-                                <a class="nav-link text-dark" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    GA-2110SC-4A
-                                </a>
+                                <h3>Collection</h3>
                             </li>
                         </ul>
                     </div>
@@ -67,7 +64,7 @@
                 <div class="kiri3">
                     <div class="container">
                         <div class="judulw">
-                            <p class="display-4 fs-2 pt-3" style="text-align: center;">Wishlist</p>
+                            <p class="display-4 fs-2 pt-3" style="text-align: center;">You Collection</p>
                         </div>
                         <hr class="my-4" style="border: 1px solid gray">
                         <div class="overall d-flex">
@@ -91,10 +88,8 @@
                                     </div>
                                 </form>
                             </div>
-                            <div class="kr mt-5" style="margin-left: 19vw;">
-                                <button type="button" class="order btn btn-dark" style="width: 12vw; border:none; box-shadow: inset 0 -3em 3em rgba(125, 125, 125, 0.1), 0 0 0 2px rgb(221, 221, 221), 0.3em 0.3em 1em rgba(128, 128, 128, 0.3);
-">ORDER</button>
-                                <div class="cancel" style="margin-top: 6vw; margin-left:10vw;">
+                            <div class="kr mt-4" style="margin-left: 19vw;">
+                                <div class="cancel" style="margin-left:10vw;">
                                     <a href="#" style="color: gray;">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
                                             fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
@@ -107,6 +102,67 @@
                         </div>
                         <hr class="my-4" style="border: 1px solid gray">
 
+                        <div class="alamat">
+                            <div class="judulw">
+                                <p class="display-4 fs-3 pt-3" style="text-align: center;">Address Sended</p>
+                            </div>
+                            <div class="bgks d-flex">
+                                <div class="isinya mt-5" style="width: 30vw;">
+                                    <form action="" method="POST">
+                                        <div class="up">
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail1" class="form-label">Name</label>
+                                                <input type="text" class="form-control" id="name"
+                                                    aria-describedby="emailHelp">
+                                            </div>
+                                        </div>
+                                        <div class="up">
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail1" class="form-label">Full Name</label>
+                                                <input type="text" class="form-control" id="nmfull"
+                                                    aria-describedby="emailHelp">
+                                            </div>
+                                        </div>
+                                        <div class="up">
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail1" class="form-label">Number Phone</label>
+                                                <input type="text" class="form-control" id="nphone"
+                                                    aria-describedby="emailHelp">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                                aria-describedby="emailHelp">
+                                        </div>
+                                        <div class="up">
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail1" class="form-label">Address</label>
+                                                <input type="text" class="form-control" id="address"
+                                                    aria-describedby="emailHelp">
+                                            </div>
+                                        </div>
+                                        <label for="exampleFormControlTextarea1">Note</label>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1"
+                                            rows="3"></textarea>
+                                    </form>
+                                </div>
+                                <div class="isinya2 mt-5 pt-4" style="margin-left: 5vw;">
+                                    <h4><b>Quantity All</b></h4>
+                                    <p class="serial pt-1">1 Product</p>
+                                    <h4><b>Subtotal All</b></h4>
+                                    <p class="serial pt-1" style="color: red;">Rp. 2.499.000</p>
+                                    <h4><b>Payment</b></h4>
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr class="my-4" style="border: 1px solid gray">
+
+                        <div class="isiorder" style="float: right;">
+                            <button type="button" class="order btn btn-dark" style="width: 12vw; border:none; box-shadow: inset 0 -3em 3em rgba(125, 125, 125, 0.1), 0 0 0 2px rgb(221, 221, 221), 0.3em 0.3em 1em rgba(128, 128, 128, 0.3);
+">ORDER</button>
+                        </div>
                     </div>
                 </div>
             </div>
