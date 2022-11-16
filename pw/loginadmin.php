45f1fb4b5a2a16d1cb8eb52b0a2da8a9c53da4c5
Aldi <aldiaafn16@gmail.com>
2022-11-16 23:36:21 +0700

    ganti judul

--- pw/loginadmin.php
+++ pw/loginadmin.php
@@ -10,7 +10,7 @@
     <!-- framework -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>WACTCHES SCRT | Online Shop</title>
+    <title>WATCHES SCRT | Online Shop</title>
     <link rel="stylesheet" href="css/styl.css">
     <link rel="shortcut icon" href="asset/favicon/logoscrt.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
