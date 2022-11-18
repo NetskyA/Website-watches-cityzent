e7fcb79c040532328d021b0c5f8e5fda7911bf48
Aldi <aldiaafn16@gmail.com>
2022-11-18 23:09:41 +0700

    panik

--- pw/contactus.php
+++ pw/contactus.php
@@ -271,44 +271,43 @@ require_once("connector.php");
             <hr class="my-4 mt-1 justify-content-center">
             <div class="isinya d-flex justify-content-center">
                 <div class="satu d-flex justify-content-center" style="text-decoration: none;">
-                    <a href="sop.php">
+                    <a href="sop.php" style="text-decoration:none; display:flex;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
                             class="bi bi-shop-window mt-3 text-warning" viewBox="0 0 16 16">
                             <path
                                 d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h12V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zm2 .5a.5.5 0 0 1 .5.5V13h8V9.5a.5.5 0 0 1 1 0V13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a.5.5 0 0 1 .5-.5z" />
                         </svg>
+                        <h3 class="jdl1 mt-4 ms-1 text-dark">
+                            Shop at scrt
+                        </h3>
                     </a>
-                    <h3 class="jdl1 mt-4 ms-1">
-                        Shop at scrt
-                    </h3>
                 </div>
                 <div class="dua d-flex justify-content-center">
-                    <a href="pay.php">
+                    <a href="pay.php" style="text-decoration:none; display:flex;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
                             class="bi bi-wallet2 mt-3 text-success" viewBox="0 0 16 16">
                             <path
                                 d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z" />
                         </svg>
+                        <h3 class="jdl1 mt-4 ms-2 text-dark">
+                            Payments
+                        </h3>
                     </a>
-                    <h3 class="jdl1 mt-4 ms-2">
-                        Payments
-                    </h3>
-
                 </div>
                 <div class="tiga d-flex justify-content-center">
-                    <a href="order.php">
+                    <a href="order.php" style="text-decoration:none; display:flex;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
                             class="bi bi-truck mt-3 text-info" viewBox="0 0 16 16">
                             <path
                                 d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                         </svg>
+                        <h4 class="jdl1 mt-4 ms-2 text-dark">
+                            Orders & Delivery
+                        </h4>
                     </a>
-                    <h4 class="jdl1 mt-4 ms-2">
-                        Orders & Delivery
-                    </h4>
                 </div>
                 <div class="empat d-flex justify-content-center">
-                    <a href="retur.php">
+                    <a href="retur.php" style="text-decoration:none; display:flex;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
                             class="bi bi-layer-backward mt-4 text-danger" viewBox="0 0 16 16">
                             <path
@@ -316,15 +315,15 @@ require_once("connector.php");
                             <path
                                 d="M1 9a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h4.5a.5.5 0 0 1 0 1H1v2h4.5a.5.5 0 0 1 0 1H1zm9.5 0a.5.5 0 0 1 0-1H15V6h-4.5a.5.5 0 0 1 0-1H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4.5z" />
                         </svg>
+                        <h3 class="jdl1 mt-4 ms-2 text-dark">
+                            Returns & Funds
+                        </h3>
                     </a>
-                    <h3 class="jdl1 mt-4 ms-2">
-                        Returns & Funds
-                    </h3>
                 </div>
             </div>
             <div class="isinya d-flex justify-content-center">
                 <div class="empat d-flex justify-content-center">
-                    <a href="price.php">
+                    <a href="price.php" style="text-decoration:none; display:flex;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
                             class="bi bi-tags mt-3 text-warning" viewBox="0 0 16 16">
                             <path
@@ -332,22 +331,22 @@ require_once("connector.php");
                             <path
                                 d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z" />
                         </svg>
+                        <h3 class="jdl1 mt-4 ms-2 text-dark">
+                            Offers & Prizes
+                        </h3>
                     </a>
-                    <h3 class="jdl1 mt-4 ms-2">
-                        Offers & Prizes
-                    </h3>
                 </div>
                 <div class="empat d-flex justify-content-center">
-                    <a href="gen.php">
+                    <a href="gen.php" style="text-decoration:none; display:flex;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
                             class="bi bi-book mt-3 text-success" viewBox="0 0 16 16">
                             <path
                                 d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
                         </svg>
+                        <h3 class="jdl1 mt-4 ms-2 text-dark">
+                            General information
+                        </h3>
                     </a>
-                    <h3 class="jdl1 mt-4 ms-2">
-                        General information
-                    </h3>
                 </div>
             </div>
             <hr class="my-4 mt-5">
