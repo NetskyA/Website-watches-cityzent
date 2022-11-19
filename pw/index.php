3e53ad5d09aa67eaf94295724585f893d071bd98
Aldi <aldiaafn16@gmail.com>
2022-11-19 21:56:08 +0700

    apa alah

--- pw/index.php
+++ pw/index.php
@@ -330,19 +330,29 @@ require_once("connector.php");
                         </ul>
                         <a href="checkout.php" role="button" class="btn">
                             <svg xmlns="http://www.w3.org/2000/svg" width="45" height="50" fill="currentColor"
-                                class="bi bi-cart4" viewBox="0 0 16 16" style="margin-right: 1vw;">
+                                class="bi bi-cart4 me-3" viewBox="0 0 16 16" style="margin-right: 0vw;">
                                 <path
                                     d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
                             </svg>
                         </a>
-                        <a href="logincus.php" role="button" class="btn me-3" style="margin-top: 0.3vw;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="37" height="42" fill="currentColor"
-                                class="bi bi-person-circle" viewBox="0 0 16 16">
-                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                <path fill-rule="evenodd"
-                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                            </svg>
-                        </a>
+                        <div class="dropdown">
+                            <button class="btn dropdown-toggle me-5" type="button" data-bs-toggle="dropdown"
+                                aria-expanded="false"><a href="logincus.php" role="button" class="btn"
+                                    style="margin-top: 0.3vw;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="37" height="42" fill="currentColor"
+                                        class="bi bi-person-circle" viewBox="0 0 16 16">
+                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                        <path fill-rule="evenodd"
+                                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                    </svg>
+                                </a>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="logincus.php">Login</a></li>
+                                <li><a class="dropdown-item" href="#">Logout</a></li>
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
             </nav>
